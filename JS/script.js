89a551f434e5971687368317ce4751d5211b587e
Ebrahim Essa Ahmed <99309335+12288987@users.noreply.github.com>
2024-12-18 21:08:38 +0300

    Push to github

---
+++ JS/script.js
@@ -0,0 +1,79 @@
+let h4Toggle1 = $('.1h');
+// console.log(h4Toggle1);
+let h4Toggle2 = $('.2h');
+let h4Toggle3 = $('.3h');
+let h4Toggle4 = $('.4h');
+let h4Toggle5 = $('.5h');
+let h4Toggle6 = $('.6h');
+let h4Toggle7 = $('.7h');
+let h4Toggle8 = $('.8h');
+let h4Toggle9 = $('.9h');
+let h4Toggle10 = $('.10h');
+
+let A1 = $('.1d');
+// console.log(A1);
+let A2 = $('.2d');
+let A3 = $('.3d');
+let A4 = $('.4d');
+let A5 = $('.5d');
+let A6 = $('.6d');
+let A7 = $('.7d');
+let A8 = $('.8d');
+let A9 = $('.9d');
+let A10 = $('.10d');
+
+// let h4Toggle = document.querySelectorAll('footer h4');
+// console.log(h4Toggle);
+// let hiddenA = document.querySelectorAll('.underline');
+// console.log(hiddenA);
+
+
+h4Toggle1.on('click', function() {
+    // h4Toggle1.css('border-bottom','0px');
+    A1.slideToggle();
+})
+
+h4Toggle2.on('click', function() {
+    // h4Toggle1.css('border-bottom','0px');
+    A2.slideToggle();
+})
+
+h4Toggle3.on('click', function() {
+    // h4Toggle1.css('border-bottom','0px');
+    A3.slideToggle();
+})
+
+h4Toggle4.on('click', function() {
+    // h4Toggle1.css('border-bottom','0px');
+    A4.slideToggle();
+})
+
+h4Toggle5.on('click', function() {
+    // h4Toggle1.css('border-bottom','0px');
+    A5.slideToggle();
+})
+
+h4Toggle6.on('click', function() {
+    // h4Toggle1.css('border-bottom','0px');
+    A6.slideToggle();
+})
+
+h4Toggle7.on('click', function() {
+    // h4Toggle1.css('border-bottom','0px');
+    A7.slideToggle();
+})
+
+h4Toggle8.on('click', function() {
+    // h4Toggle1.css('border-bottom','0px');
+    A8.slideToggle();
+})
+
+h4Toggle9.on('click', function() {
+    // h4Toggle1.css('border-bottom','0px');
+    A9.slideToggle();
+})
+
+h4Toggle10.on('click', function() {
+    // h4Toggle1.css('border-bottom','0px');
+    A10.slideToggle();
+})
